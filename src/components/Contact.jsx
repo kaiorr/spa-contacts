b15b003ee8e78e316b2eff45957ee992ad392e54
kaiorr <kaio.rocharibeiro@gmail.com>
2020-05-18 21:18:06 -0300

    fix: componente de contato refeito com datas formatadas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-
+import React from "react"
+import { format } from "date-fns"
 import './Contact.scss';
 
-function ContactImage(props) {
-  return (
-    <span className="contact__avatar">
-      <img src={props.image} alt={props.name}></img>
-    </span>
-  );
-}
+class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.contact = props.data
+    this.isTitle = props.isTitle
+  }
 
-function ContactText(props) {
-  return (
-  <span className='contact__data'>{props.value}</span>
-  );
-}
-
-export default function Contact(props) {
-  return (
-      <article className="contact" data-testid="contact">
-        <ContactImage image={props.data.avatar} name={props.data.name} />
-        <ContactText value={props.data.name} />
-        <ContactText value={props.data.phone} />
-        <ContactText value={props.data.country} />
-        <ContactText value={props.data.admissionDate} />
-        <ContactText value={props.data.company} />
-        <ContactText value={props.data.department} />
+  render() {
+    return (
+      <article data-testid="contact" className="contact">
+        <span className="contact__avatar">
+          <img src={this.contact.avatar} alt="" />
+        </span>
+        <span data-testid="contact-name" className="contact__data">
+          {this.contact.name}
+        </span>
+        <span data-testid="contact-phone" className="contact__data">
+          {this.contact.phone}
+        </span>
+        <span data-testid="contact-country" className="contact__data">
+          {this.contact.country}
+        </span>
+        <span data-testid="contact-date" className="contact__data">
+          {this.isTitle
+            ? this.contact.admissionDate
+            : format(new Date(this.contact.admissionDate), "dd/MM/yyyy")}
+        </span>
+        <span data-testid="contact-company" className="contact__data">
+          {this.contact.company}
+        </span>
+        <span data-testid="contact-department" className="contact__data">
+          {this.contact.department}
+        </span>
       </article>
-  );
+    )
+  }
 }
+
+export default Contact
